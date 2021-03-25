88197a5c165436b72a826c8bb0a0989802548285
Kiss, Aron <aron123dev@gmail.com>
2021-03-25 17:43:10 +0100

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_4cabe539b470_key -iv $encrypted_4cabe539b470_iv -in ./deploy_key.enc -out ./deploy_key -d
+openssl aes-256-cbc -K $encrypted_4cabe539b470_key -iv $encrypted_4cabe539b470_iv -in ./deployment/deploy_key.enc -out ./deployment/deploy_key -d
 
 echo "Uploading controller/"
 echo "Uploading service/"
