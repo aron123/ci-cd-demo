3ccbff21bb477fd11ecbbec90c7db2f6c74ad8f1
Kiss, Aron <aron123dev@gmail.com>
2021-03-25 18:06:03 +0100

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 openssl aes-256-cbc -K $encrypted_4cabe539b470_key -iv $encrypted_4cabe539b470_iv -in ./deployment/deploy_key.enc -out ./deployment/deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ./deployment/deploy_key
+echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./deployment/deploy_key
 
 echo "Uploading controller/"
 echo "Uploading service/"
@@ -11,4 +15,4 @@ echo "Uploading package-lock.json"
 
 # ssh: npm install
 
-# ssh: pm2 restart calculator 
+ssh -i "./deployment/deploy_key" $SERVER_USER@$SERVER_IP pm2 restart calculator
