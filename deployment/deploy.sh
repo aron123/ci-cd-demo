dee456eb370a4fca988c3138d1897be95edaec99
Kiss, Aron <aron123dev@gmail.com>
2021-03-25 17:10:14 +0100

    Introduce CD

---
+++ deployment/deploy.sh
@@ -0,0 +1,10 @@
+echo "Uploading controller/"
+echo "Uploading service/"
+echo "Uploading static/"
+echo "Uploading index.js"
+echo "Uploading package.json"
+echo "Uploading package-lock.json"
+
+# ssh: npm install
+
+# ssh: pm2 restart calculator 
