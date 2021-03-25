c3589e874934ff6282c55ebb6322afb325906acc
Kiss, Aron <aron123dev@gmail.com>
2021-03-25 17:14:19 +0100

    Add key decryption to deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,5 @@
+openssl aes-256-cbc -K $encrypted_4cabe539b470_key -iv $encrypted_4cabe539b470_iv -in ./deploy_key.enc -out ./deploy_key -d
+
 echo "Uploading controller/"
 echo "Uploading service/"
 echo "Uploading static/"
